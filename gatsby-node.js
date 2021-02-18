d6e87f8be4848e7b6f1fc975fda5ac3091c52ea0
mgt <mgt@oi-wiki.org>
2021-02-18 17:42:12 +0800

    add swc

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,42 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          // `.swcrc` can be used to configure swc
+          loader: "swc-loader",
+          options: {
+              "jsc": {
+                "parser": {
+                  "syntax": "ecmascript",
+                  "jsx": true,
+                  "dynamicImport": true,
+                  "privateMethod": true,
+                  "functionBind": true,
+                  "exportDefaultFrom": true,
+                  "exportNamespaceFrom": true,
+                  "decorators": true,
+                  "decoratorsBeforeExport": true,
+                  "topLevelAwait": true,
+                  "importMeta": true
+                }
+              }
+          }
+        }
+      }
+    ]
+  }
+  })
+}
